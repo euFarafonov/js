115d425c883ee3af7fbfd80aa1e3d5dde0512a4e
Eugene Farafonov <eu.farafonov@gmail.com>
2018-07-09 14:28:56 +0300

    clear task2

--- task1/ImprovedFighter.js
+++
@@ -1,7 +0,0 @@
-import {Fighter} from "./fighter";
-
-export class ImprovedFighter extends Fighter {
-	doubleHit(enemy, point) {
-		super.hit(enemy, 2 * point);
-	}
-}
--- task1/index.js
+++
@@ -1,9 +0,0 @@
-import "babel-polyfill";
-import {Fighter} from "./fighter";
-import {ImprovedFighter} from "./improvedFighter";
-import {Fight} from "./fight";
-
-let fighter = new Fighter("fighter", 100, 10);
-let iFighter = new ImprovedFighter("iFighter", 100, 20);
-
-Fight(fighter, iFighter, 5, 10, 15);
